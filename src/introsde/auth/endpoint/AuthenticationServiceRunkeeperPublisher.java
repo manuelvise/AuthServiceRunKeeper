957de75adb6754e632cb57de11d7a514adfe61e7
manuelvise <manuelvise@gmail.com>
2015-01-24 11:41:30 +0100

    get auth code, get access token, get url authentication.

---
+++ src/introsde/auth/endpoint/AuthenticationServiceRunkeeperPublisher.java
@@ -0,0 +1,22 @@
+package introsde.auth.endpoint;
+import introsde.auth.ws.AuthenticationServiceRunkeeperImpl;
+
+import javax.xml.ws.Endpoint;
+
+public class AuthenticationServiceRunkeeperPublisher {
+	public static String SERVER_URL = "http://localhost";
+	public static String PORT = "6901";
+	public static String BASE_URL = "/ws/lifecoach/authenticationrunkeeper";
+	
+	public static String getEndpointURL() {
+		return SERVER_URL+":"+PORT+BASE_URL;
+	}
+ 
+	public static void main(String[] args) {
+		String endpointUrl = getEndpointURL();
+		System.out.println("Starting Runkeeper authentication Service...");
+		System.out.println("--> Published at = "+endpointUrl);
+		Endpoint.publish(endpointUrl, new AuthenticationServiceRunkeeperImpl());
+    }
+}
+
